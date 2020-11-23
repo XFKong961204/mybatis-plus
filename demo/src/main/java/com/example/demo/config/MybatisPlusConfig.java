27e1c0832eefd4c3bb2ae671521436fb5162d6e4
kongxiangfei <xfkong961204@163.com>
2020-11-23 16:53:09 +0800

    mybatis-plus练习

---
+++ demo/src/main/java/com/example/demo/config/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@MapperScan("com.example.demo.mapper")
+@Configuration
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+
+    @Bean
+    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor(){
+        return  new OptimisticLockerInnerInterceptor();
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
+        return mybatisPlusInterceptor;
+    }
+}
---
+++ demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private Integer age;
+
+    private String name;
+
+    private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    @Version
+    private Integer version;
+
+    @TableLogic
+    private Integer deleted;
+}
